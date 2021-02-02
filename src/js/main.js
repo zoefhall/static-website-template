a20c90d48c669195a7aab1363c3bbcaae052fa0f
Zoe Hall <zoe.woodward@egov.com>
2021-02-02 16:44:30 -0500

    Accessible Slider
    Functioning accessible slider, needs styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import { logSomething } from "./module";
 
-logSomething("Hello World");
+logSomething(" ");
+
+$(document).ready(function() {
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
